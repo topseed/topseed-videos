8173e90d155227fcbca04f64b633eb55a721746f
puppetmaster3 <vic@vics-MacBook-Air.local>
2017-02-23 19:32:28 -0500

    tsc.

--- mserv/index.js
+++ mserv/index.js
@@ -1,27 +1,25 @@
-'use strict';
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const middle = express()
-middle.use(bodyParser.json()) // parse application/json
-middle.use(cors())
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const middle = express();
+middle.use(bodyParser.json()); // parse application/json
+const compression = require("compression");
+middle.use(compression());
+middle.use(cors());
 // ###################### 
-
 middle.get('/', function (req, res) {
-  res.send('nothing to see here')
-})
-
+    res.send('nothing to see here');
+});
 //routes ###################### 
-const membersPg = require('./route/membersPg')
-middle.use('/membersPg', membersPg) //front route 1 - match the front end
-
+const membersPg = require("./route/membersPg");
+middle.use('/membersPg', membersPg); //front route 1 - match the front end
 //###################### 
 // start the service
-const PORT = 8083
-middle.listen(PORT, '0.0.0.0', function() {
-	console.log('App listening on port')
-	console.log('Press Ctrl+C to quit.')
-})
-
-
-
+const PORT = 8083;
+middle.listen(PORT, '0.0.0.0', function () {
+    console.log('App listening on port ' + PORT);
+    console.log('Press Ctrl+C to quit.');
+});
+//# sourceMappingURL=index.js.map
