98ddc11910d9a0d4a17e3e80608447f5d3a7d677
puppetmaster3 <vicmasons@gmail.com>
2017-03-30 11:07:07 -0400

    !important

--- tut/nav/web-app/index.js
+++ tut/nav/web-app/index.js
@@ -5,6 +5,14 @@ const server = express()
 // ###################### static
 server.use(express.static('webroot'))
 
+// optional:
+const scribe = require('scribe-js')()
+const console = process.console
+server.use(scribe.express.logger()) //Log each request for now
+server.use('/logs', scribe.webPanel())
+const debug = require('debug')('my-app')
+debug('oh hi')
+
 //###################### start the server
 const PORT = 8080
 server.listen(PORT, '0.0.0.0', function() {
