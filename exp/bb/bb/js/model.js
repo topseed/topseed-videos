ebae25584fa008fe3e98a7aebf909cc30a78f5ed
puppetmaster3 <vicmasons@gmail.com>
2017-04-08 18:25:05 -0400

    touch

---
+++ exp/bb/bb/js/model.js
@@ -0,0 +1,12 @@
+﻿var Book = Backbone.Model.extend({
+	defaults: {
+		ID: "",
+		BookName: ""
+	},
+
+	idAttribute: "ID"
+});
+
+var BooksCollection = Backbone.Collection.extend({
+	model: Book
+});
